ddf28e4116f9d746803fdef3299c819b810ae338
SadickYahaya <sadickashton@live.com>
2025-03-23 12:56:19 +0000

    success modal after order

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -116,7 +116,7 @@ onMounted(() => {
 
 .hero-section {
   background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-              url('/hero.jpg') center/cover;
+              url('/hero4.jpg') center/cover;
   color: white;
   text-align: center;
   padding: 4rem 2rem;
